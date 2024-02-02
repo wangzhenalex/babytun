6381433381c85b9baf70ed20289ebfa05fb15a0e
zhen wang <wangzhen_1990@163.com>
2024-02-02 23:39:39 +0800

    Redis解决超发问题

--- babytun-seckill/src/main/java/org/example/babytunseckill/services/PromotionSecKillService.java
+++ babytun-seckill/src/main/java/org/example/babytunseckill/services/PromotionSecKillService.java
@@ -16,7 +16,7 @@ import java.util.Map;
 import java.util.UUID;
 
 @Service
-public class PromotionSeckillService {
+public class PromotionSecKillService {
     @Resource
     private PromotionSecKillDAO promotionSecKillDAO;
     @Resource
@@ -58,13 +58,13 @@ public class PromotionSeckillService {
     public String sendOrderToQueue(String userid) {
         System.out.println("准备向队列发送信息");
         //订单基本信息
-        Map data = new HashMap();
-        data.put("userid", userid);
-        String orderNo = UUID.randomUUID().toString();
-        data.put("orderNo", orderNo);
-        //附加额外的订单信息
-        rabbitTemplate.convertAndSend("exchange-order" , null , data);
-        return orderNo;
+//        Map data = new HashMap();
+//        data.put("userid", userid);
+//        String orderNo = UUID.randomUUID().toString();
+//        data.put("orderNo", orderNo);
+//        //附加额外的订单信息
+//        rabbitTemplate.convertAndSend("exchange-order" , null , data);
+        return "orderNo";
     }
 
     public Order checkOrder(String orderNo){
